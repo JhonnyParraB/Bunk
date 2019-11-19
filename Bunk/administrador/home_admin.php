f2b22d80a64ad5a7cf858c45c3384161829397ec
Laura Donado <ldonado11@gmail.com>
2019-11-19 17:24:03 -0500

    administrar bancos listo

---
+++ Bunk/administrador/home_admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Admin</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <h1>Bienvenido Admin</h1>
+        <ul>
+            <li><a href="banco/listar_bancos.php">Administrar bancos</a></li>
+            <li><a href="#">Gestionar usuarios</a></li>
+            <li><a href="#">Editar cuentas</a></li>
+            <li><a href="#">Editar créditos</a></li>
+            <li><a href="fin_de_mes.php">Fin de mes</a></li>
+            <li><a href="centro_mensajes.php">Centro de mensajes</a></li>
+        </ul>
+    </body>
+</html>
