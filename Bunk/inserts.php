692ed8db66984338eadd89723e798884819e77b8
Laura Donado <ldonado11@gmail.com>
2019-11-19 22:48:01 -0500

    Update inserts.php

--- Bunk/inserts.php
+++ Bunk/inserts.php
@@ -19,7 +19,10 @@
             values('Kevin','cliente3','kevin@gmail.com','Kevin','Pelaez',1234527);
             
             insert into Administradores (usuario,contrasena)
-            values('admin','admin');";
+            values('admin','admin');
+            INSERT INTO CUENTAS_AHORRO (id, saldo, cliente_id, cuota_manejo, banco_id) VALUES ('1', '1000', '1', '1', '1'), ('2', '500', '3', '1', '2');
+            INSERT INTO CREDITOS (id, tasa_interes, fecha_pago, valor, dias_mora, aprobado, pagado, cliente_id, visitante_id, fecha_aprobacion, fecha_solicitud, banco_id, mensaje) VALUES ('1', '1', '2019-11-20', '1000', '0', b'0', b'0', '1', NULL, NULL, '2019-11-18 00:00:00', '2', NULL);
+            INSERT INTO TARJETAS_CREDITO (id, aprobada, cupo_maximo, cuota_manejo, tasa_interes, sobrecupo, cuenta_ahorro_id, fecha_aprobacion, fecha_solicitud, mensaje) VALUES ('1', b'0', NULL, NULL, NULL, NULL, '1', NULL, '2019-11-11 00:00:00', NULL);";
     if(mysqli_multi_query($con,$sql)){
         echo "Tablas creadas<br>";
     }else{
