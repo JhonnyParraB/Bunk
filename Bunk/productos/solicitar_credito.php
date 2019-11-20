a4d6c2bf101d6dbbd491040a0fa4e845ac7734be
JhonnyParraB <jhonnyparra01@hotmail.com>
2019-11-19 21:21:18 -0500

    Solicitar un credito completo

--- Bunk/productos/solicitar_credito.php
+++ Bunk/productos/solicitar_credito.php
@@ -1,138 +1,145 @@
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-    </head>
-    <body>
-        <h1>Solicitar Credito</h1>
 
-        <?php 
-            include_once dirname(__FILE__) . '/../config.php';
-            include_once dirname(__FILE__) . '/../utils/utils.php';
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
 
+<body>
+    <h1>Solicitar Credito</h1>
 
-            $con = mysqli_connect(HOST_DB, USUARIO_DB, USUARIO_PASS, NOMBRE_DB);
-            $sql = 'SELECT * FROM BANCOS';
-            $resultado = mysqli_query($con , $sql);
-            $bancos = array();
-            while ($fila = mysqli_fetch_array($resultado)){
-                $bancos["'".$fila['id']."'"] = $fila['nombre'];
-            }
-            
-
-            $formularioSolicitarCredito = "";
-            $formularioSolicitarCredito .= '<form action="abrir_cuenta_ahorros.php" method="post">';
-            $formularioSolicitarCredito .= crearSelect('Banco', 'banco', $bancos);
+    <?php
+    include_once dirname(__FILE__) . '/../config.php';
+    include_once dirname(__FILE__) . '/../utils/utils.php';
 
 
-            //ESTO SE DEBE EXTRAER DE LA SESIÓN
-            $usuarioAutenticado = true;
-            $formulario = "";
-            if ($usuarioAutenticado){
-                $formulario = array(
-                    array('Tasa de interés propuesta', 'tasa_interes_propuesta', 'number', ''),
-                    array('Fecha de pago', 'fecha_pago', 'date', ''),
-                    array('Valor', 'valor', 'javecoin', ''),
-                    array('Solicitar Crédito', 'solicitar_credito', 'submit', '')
-                );
-            }
-            else{
-                $formulario = array(
-                    array('Cedula', 'cedula', 'number', ''),
-                    array('E-Mail', 'email', 'email', ''),
-                    array('Fecha de pago', 'fecha_pago', 'date', ''),
-                    array('Valor', 'valor', 'javecoin', ''),
-                    array('Solicitar Crédito', 'solicitar_credito', 'submit', '')
-                );
-            }
-
-           
-
-            $formularioSolicitarCredito .= crearFormulario2($formulario);
-            $formularioSolicitarCredito .= '</form>';
-            echo $formularioSolicitarCredito;
-
-            if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-                
-                
-                if (isset($_POST['solicitar_credito'])){
-                    $banco_id = $_POST['banco'];
-                    
-
-                    $sql = "SELECT * FROM BANCOS WHERE id=$banco_id";
-                    $resultado = mysqli_query($con , $sql);
-                    $row = mysqli_fetch_array($resultado);
-
-                    $interes_credito_visitantes = $row['interes_credito_visitantes'];
-                    $nombre_banco = $row['nombre'];
-
-                    //ESTO DEBERIA EXTRAERSE DE LA SESIÓN
-                    $usuarioAutenticado = true;
-                    $valor = $_POST['valor'];
-                    $fecha_pago = $_POST['fecha_pago'];
+    $con = mysqli_connect(HOST_DB, USUARIO_DB, USUARIO_PASS, NOMBRE_DB);
+    $sql = 'SELECT * FROM BANCOS';
+    $resultado = mysqli_query($con, $sql);
+    $bancos = array();
+    while ($fila = mysqli_fetch_array($resultado)) {
+        $bancos["'" . $fila['id'] . "'"] = $fila['nombre'];
+    }
 
 
-                    
-                    if($usuarioAutenticado){
-                        //ESTO DEBERIA EXTRAERSE DE LA SESIÓN
-                        $cliente_id = 1;
-                        $cliente =         23;            
-
-                        
+    $formularioSolicitarCredito = "";
+    $formularioSolicitarCredito .= '<form action="solicitar_credito.php" method="post">';
+    $formularioSolicitarCredito .= crearSelect('Banco', 'banco', $bancos);
 
 
-                    }else{
-                        
-                        $cedula = $_POST['cedula'];
-                        $email = $_POST['email'];
-
-                        $sql = "SELECT * FROM VISITANTES WHERE Cedula = '$cedula'";
-                        $resultado = mysqli_query($con, $sql);
-                        if (mysqli_num_rows($resultado) > 0) {
-                            $sql = "UPDATE * FROM VISITANTES 
-                                    SET email='$email'
-                                    WHERE Cedula = '$cedula'";
-                        }
-                        else{
-                            $sql = "INSERT INTO VISITANTES (cedula, email)
-                                    VALUES ($cedula, $email)";
-                        }
-                        mysqli_query($con, $sql);
-                        $sql = "SELECT * FROM VISITANTES WHERE Cedula = '$cedula'";
-                        $resultado = mysqli_query($con , $sql);
-                        $row = mysqli_fetch_array($resultado);
-                        $visitante_id = $row['id'];
-
-                        $sql = "INSERT INTO CREDITOS (tasa_interes, fecha_pago, valor, visitante_id)
-                                VALUES ($interes_credito_visitantes, $fecha_pago, $valor, $visitante_id)";
+    //ESTO SE DEBE EXTRAER DE LA SESIÓN
+    $usuarioAutenticado = false;
+    $formulario = "";
+    if ($usuarioAutenticado) {
+        $formulario = array(
+            array('Tasa de interés propuesta', 'tasa_interes_propuesta', 'number', ''),
+            array('Fecha de pago', 'fecha_pago', 'date', ''),
+            array('Valor', 'valor', 'javecoin', ''),
+            array('Solicitar Crédito', 'solicitar_credito', 'submit', '')
+        );
+    } else {
+        $formulario = array(
+            array('Cedula', 'cedula', 'number', ''),
+            array('E-Mail', 'email', 'email', ''),
+            array('Fecha de pago', 'fecha_pago', 'date', ''),
+            array('Valor', 'valor', 'javecoin', ''),
+            array('Solicitar Crédito', 'solicitar_credito', 'submit', '')
+        );
+    }
 
 
 
+    $formularioSolicitarCredito .= crearFormulario2($formulario);
+    $formularioSolicitarCredito .= '</form>';
+    echo $formularioSolicitarCredito;
 
-                        
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
-                    }
 
-                    $saldo_inial = 0;
-                    //ESTO SE DEBE EXTRAER DE LA SESIÓN:
-                    $cliente_id = 1;
+        if (isset($_POST['solicitar_credito'])) {
 
-                     
 
-                    $sql = "INSERT INTO CREDITOS (saldo, cliente_id, cuota_manejo, banco_id) VALUES ($saldo_inial, $cliente_id, $cuota_manejo, $banco_id)";
-                    if (mysqli_query($con, $sql)) {
-                        echo "Se ha abierto una cuenta de ahorros en el banco $nombre_banco, la cuota de manejo es $cuota_manejo JaveCoins y la tasa de interes es $interes_cuenta_ahorros%.</br>";
-                        echo "El saldo inicial de la cuenta es 0 JaveCoins";
-                    } else {
-                        echo 'Hubo un error al intentar abrir la cuenta de ahorros '.mysqli_error($con);
-                    }
+            $banco_id = $_POST['banco'];
+
+
+            $sql = "SELECT * FROM BANCOS WHERE id=$banco_id";
+            $resultado = mysqli_query($con, $sql);
+            $row = mysqli_fetch_array($resultado);
+
+            $interes_credito_visitantes = $row['interes_credito_visitantes'];
+            $nombre_banco = $row['nombre'];
+
+            //ESTO DEBERIA EXTRAERSE DE LA SESIÓN
+            $usuarioAutenticado = false;
+            $valor = $_POST['valor'];
+            $fecha_pago = $_POST['fecha_pago'];
+
+            //POR DEFECTO
+            $tasa_interes = $interes_credito_visitantes;
+            $cedula = "";
+            $email = "";
+
+            date_default_timezone_set('America/Bogota');
+            $fecha_solicitud = date('Y-m-d H:i:s', time());
+
+
+            if ($usuarioAutenticado) {
+                //ESTO DEBERIA EXTRAERSE DE LA SESIÓN
+                $cliente_id = 1;
+                $sql = "SELECT * FROM CLIENTES WHERE id = $cliente_id";
+                $resultado = mysqli_query($con, $sql);
+                $row = mysqli_fetch_array($resultado);
+                $cedula = $row['cedula'];
+                $email = $row['email'];
+
+                if (isset($_POST['tasa_interes_propuesta'])) {
+                    $tasa_interes = $_POST['tasa_interes_propuesta'];
                 }
+
+                $sql = "INSERT INTO CREDITOS (tasa_interes, fecha_pago, valor, cliente_id, banco_id, fecha_solicitud)
+                                VALUES ($tasa_interes, '$fecha_pago', $valor, $cliente_id, $banco_id, '$fecha_solicitud')";
+            } else {
+
+                $cedula = $_POST['cedula'];
+                $email = $_POST['email'];
+
+
+                $sql = "SELECT * FROM VISITANTES WHERE Cedula = '$cedula'";
+                $resultado = mysqli_query($con, $sql);
+                if (mysqli_num_rows($resultado) > 0) {
+                    $sql = "UPDATE VISITANTES
+                                    SET email='$email'
+                                    WHERE cedula = $cedula";
+                } else {
+                    $sql = "INSERT INTO VISITANTES (cedula, email)
+                                    VALUES ($cedula, '$email')";
+                }
+                mysqli_query($con, $sql);
+                $sql = "SELECT * FROM VISITANTES WHERE Cedula = '$cedula'";
+                $resultado = mysqli_query($con, $sql);
+                $row = mysqli_fetch_array($resultado);
+                $visitante_id = $row['id'];
+
+                $sql = "INSERT INTO CREDITOS (tasa_interes, fecha_pago, valor, visitante_id, banco_id, fecha_solicitud)
+                                VALUES ($tasa_interes, '$fecha_pago', $valor, $visitante_id, $banco_id, '$fecha_solicitud')";
             }
 
-        ?>
-    </body>
+
+
+
+            if (mysqli_query($con, $sql)) {
+                echo "Se ha solicitado el crédito al banco $nombre_banco con una tasa de interés de $tasa_interes%.</br>";
+                echo "Su solicitud será evaluada por un administrador, se le enviará un correo con la aprobación o rechazo.";
+            } else {
+                echo 'Hubo un error al solicitar su credito: ' . mysqli_error($con);
+            }
+        }
+    }
+
+    ?>
+</body>
+
 </html>
