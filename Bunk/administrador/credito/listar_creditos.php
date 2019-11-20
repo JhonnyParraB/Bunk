15f240f045c4d0a39b3300ebc2ce504bd20f0cd0
Laura Donado <ldonado11@gmail.com>
2019-11-19 21:21:02 -0500

    implementado gestionar creditos

---
+++ Bunk/administrador/credito/listar_creditos.php
@@ -0,0 +1,69 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Créditos</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    </head>
+    <body>
+        <h1>Créditos</h1>
+        <?php
+            include_once '../../config.php';
+            $con = mysqli_connect(HOST_DB, USUARIO_DB, USUARIO_PASS, NOMBRE_DB);
+            $datos = "";
+            if(mysqli_connect_errno()){
+                $datos.= "Error en la conexión: ".mysqli_conecct_error()."<br>";
+            }
+        ?>
+        <table class="table">
+            <tr>
+                <th>Banco</th>
+                <th>Cédula</th>
+                <th>Email</th>
+                <th>Valor</th>
+                <th>Tasa Interés</th>
+                <th>Fecha de pago</th>
+                <th>Dias de mora</th>
+                <th>Pagado</th>
+                <th>Fecha de aprobación</th>
+                <th></th>
+            </tr>
+        <?php
+            $sql = "SELECT cr.id, b.nombre, c.cedula, c.email, cr.valor, cr.tasa_interes, cr.fecha_pago, 
+                            cr.dias_mora, cr.pagado, cr.fecha_aprobacion
+                    FROM Clientes c, Bancos b, creditos cr
+                    WHERE b.id = cr.banco_id AND c.id=cr.cliente_id AND cr.aprobado=1";
+            $resultado = mysqli_query($con,$sql);
+            while($fila = mysqli_fetch_array($resultado)){
+                $datos.='<tr>';
+                for($i =1; $i <=9; ++$i){
+                    $datos.="<td>".($i==8? ($fila[$i]==1? 'Pagado': 'Debe'):$fila[$i])."</td>";
+                }
+                $datos.="<td><a href=".$_SERVER['PHP_SELF']."?credito=".$fila[0].">Editar</td>";
+                $datos.='</tr>';
+            }
+            $sql = "SELECT cr.id, b.nombre, v.cedula, v.email, cr.valor, cr.tasa_interes, cr.fecha_pago, 
+                            cr.dias_mora, cr.pagado, cr.fecha_aprobacion
+                    FROM visitantes v, Bancos b, creditos cr
+                    WHERE b.id = cr.banco_id AND v.id=cr.visitante_id AND cr.aprobado=1";
+            $resultado = mysqli_query($con,$sql);
+            while($fila = mysqli_fetch_array($resultado)){
+                $datos.='<tr>';
+                for($i =1; $i <=9; ++$i){
+                    $datos.="<td>".($i==8? ($fila[$i]==1? 'Pagado': 'Debe'):$fila[$i])."</td>";
+                }
+                $datos.="<td><a href=".$_SERVER['PHP_SELF']."?credito=".$fila[0].">Editar</td>";
+                $datos.='</tr>';
+            }
+            $datos.='</table>';
+            echo $datos;
+            if(isset($_GET['credito'])){
+                $_SESSION['credito']=$_GET['credito'];
+                header('Location: editar_credito.php');
+            }
+        ?>
+    </body>
+</html>
